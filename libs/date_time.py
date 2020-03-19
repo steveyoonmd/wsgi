acce7e58e2c41d8794cf7e1441a93ec53a233872
Steve Yoon <steve.yoon@marketdesigners.com>
2020-03-19 18:24:14 +0900

    trivial

--- libs/date_time.py
+++ libs/date_time.py
@@ -1,9 +1,11 @@
-from datetime import timedelta, timezone
+from datetime import datetime, timedelta, timezone
 
 
 class DateTime:
-    def __init__(self, dt, time_zone='UTC'):
+    def __init__(self, dt=None, time_zone='UTC'):
         self.dt = dt
+        if self.dt is None:
+            self.dt = datetime.utcnow()
         if time_zone == 'KST':
             self.dt = self.dt + timedelta(hours=9)
 
